602bf13aaa843fbe0d2b5b963023c2d93a05870a
ushivakumar855 <pes1ug23cs659@pesu.pes.edu>
2026-02-13 16:18:11 +0530

    Keep only movies2.txt

--- peek.py
+++
@@ -1,6 +0,0 @@
-try:
-    with open("movies2.txt", "r", encoding="utf-8", errors="ignore") as f:
-        for i in range(5):
-            print(repr(f.readline()))
-except Exception as e:
-    print(e)
